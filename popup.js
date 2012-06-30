8a8ab34239726d9c164218144a055968e2e17bdc
Martynas Mickevičius <martynas@2m.lt>
2012-06-30 22:53:59 +0300

    do not allow empty show title

--- popup.js
+++ popup.js
@@ -64,7 +64,7 @@ function addShow() {
     var showTitle = $("#addShow #showTitle").val().replace(/[^A-Za-z0-9 -]/g, '');
     var showId = showTitle.replace(/ /g, '_');
     
-    if (!getBackgroundHandle().alreadyAdded(showId)) {
+    if (showId != "" && !getBackgroundHandle().alreadyAdded(showId)) {
         var showData = getBackgroundHandle().getNewShowData(showTitle);
 
         $("#addShow #showTitle").attr("disabled", "disabled");

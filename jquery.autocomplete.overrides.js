8e255a982af29abdea835f3c32616d8cd96989ee
Martynas Mickevičius <martynas@2m.lt>
2012-06-30 11:32:16 +0300

    handle automatic redirects when searching

--- jquery.autocomplete.overrides.js
+++ jquery.autocomplete.overrides.js
@@ -19,24 +19,33 @@
 			house
 			klaus
 	*/
-	function parseResponse(response) {
-		var titleRegExp = new RegExp('"><b>(.*)</b></a>', "g");
-		var yearRegExp = new RegExp('</b></a> ([()0-9-]*)<br>', "g");
-		var statusRegExp = new RegExp('<td align="left" nowrap>(.*)</td></tr>', "g");
-		var idRegExp = new RegExp('<a href="/show/(.*)"><b>', "g");
-		
+    function parseResponse(response) {
+        var redirectRegExp = new RegExp('Show Summary', "g");
+        
+        if (null != redirectRegExp.exec(response)) {
+            var titleRegExp = new RegExp('nowrap>&nbsp;<h1>(.*)</h1></th>', "g");
+            var yearRegExp = new RegExp('<th width="70" align="center" valign="middle" nowrap>(.*)</th>', "g");
+            var statusRegExp = new RegExp('<th width="170" align="center" valign="middle" nowrap>(.*)</th>', "g");
+            var idRegExp = new RegExp('<a href="/news/(.*)/" class="blueheader">News', "g");
+        }
+        else {
+            var titleRegExp = new RegExp('"><b>(.*)</b></a>', "g");
+            var yearRegExp = new RegExp('</b></a> ([()0-9-]*)<br>', "g");
+            var statusRegExp = new RegExp('<td align="left" nowrap>(.*)</td></tr>', "g");
+            var idRegExp = new RegExp('<a href="/show/(.*)"><b>', "g");
+        }
+
 		var returnString = "";
-		
-		var title;
-		while ((title = titleRegExp.exec(response))) {
-			var year = yearRegExp.exec(response);
-			var status = statusRegExp.exec(response);
-			var id = idRegExp.exec(response);
-		
-			returnString += title[1] + "|" + year[1] + "|" + status[1] + "|" + id[1] + "\n";			
-		}
-		
-		console.log(returnString);
+        
+        var title;
+        while ((title = titleRegExp.exec(response))) {
+            var year = yearRegExp.exec(response);
+            var status = statusRegExp.exec(response);
+            var id = idRegExp.exec(response);
+        
+            returnString += title[1] + "|" + year[1] + "|" + status[1] + "|" + id[1] + "\n";			
+        }
+
 		return returnString;
 	}
 	
